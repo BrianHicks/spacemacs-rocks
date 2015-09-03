f06442fc654ad607754cbb5579c822b4e4e41453
Brian Hicks <brian@brianthicks.com>
2015-09-03 11:42:04 -0500

    travis: finish working config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-set -x
-git pull
-
-scripts/publish.sh
-STATUS=$?
 set -e
+chmod 600 ~/.ssh/id_rsa
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+git remote add deploy git@github.com:BrianHicks/spacemacs-rocks.git
+git fetch deploy gh-pages
+
+scripts/publish.sh && STATUS=0 || STATUS=$?
 
 if [[ $STATUS == 0 ]]; then
-    eval $(ssh-agent -s)
-    chmod 600 ~/.ssh/id_rsa
-    ssh-add ~/.ssh/id_rsa
-    git remote add deploy git@github.com:BrianHicks/spacemacs-rocks.git
     git push deploy gh-pages
     git remote remove deploy
+fi
+
+# 1 is the error status from publish, so leave it
+
+if [[ $STATUS == 2 ]]; then
+    echo "no changes made to push"
 fi
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 if [[ "$(git status --porcelain)" != "" ]]; then
     echo publish should only be run on a clean working directory
     exit 1
 fi
 
-TEMP=$(mktemp -d -t spacemacs-rocks)
+TEMP=$(mktemp -d -t spacemacs-rocks.XXXX)
 git submodule update
 hugo -d $TEMP
 MESSAGE="$(git show --abbrev-commit --oneline --no-patch) (update from master content)"
 
 # move to gh-pages to place content
-git co gh-pages
-git pull
+git checkout gh-pages
 
 # remove all the content to let the rendered replace it
 git ls-files | grep -v -e README.md -e .gitignore -e CNAME | xargs rm -rf
@@ -21,9 +20,8 @@ find * -empty -type d -delete
 mv ${TEMP}/* .
 rmdir $TEMP
 
-git add .
-git ci -m "$MESSAGE" || exit 2
-# git push origin gh-pages
+git add -A .
+git commit -m "$MESSAGE" || exit 2
 
 # restore to master
-git co master
+git checkout master
